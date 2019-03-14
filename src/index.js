90f6d4814386919be520a74e3e5b6a18570d8160
mdave16 <mdave3927@gmail.com>
2019-03-14 00:06:06 +0000

    working hello component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 // create a new component
 // which will output html
-
+const App = () => {
+	return <div>i work now</div>
+}
 // gonna put it on the page/dom
+ReactDOM.render(<App />, document.querySelector('.container'))
