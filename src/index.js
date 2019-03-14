50baf3b58c488bcc6bcbd55f8c997226e30d90ed
mdave16 <mdave3927@gmail.com>
2019-03-14 00:24:30 +0000

    add api key

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // create a new component
 // which will output html
+
+const API_KEY='AIzaSyCcFGlxgtxuAh_YdydQEaIj64qCDe4k6gY'
+
 const App = () => {
 	return <div>i work now</div>
 }
