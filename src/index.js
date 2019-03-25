8eb93c600ad8326116ecfbdd727b25ed64f5d0bd
mdave16 <mdave3927@gmail.com>
2019-03-25 22:56:04 +0000

    create and export your first component!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// create a new component
-// which will output html
+import SearchBar from './components/search_bar'
 
 const API_KEY='AIzaSyCcFGlxgtxuAh_YdydQEaIj64qCDe4k6gY'
 
 const App = () => {
-	return <div>i work now</div>
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	)
 }
-// gonna put it on the page/dom
+
 ReactDOM.render(<App />, document.querySelector('.container'))
